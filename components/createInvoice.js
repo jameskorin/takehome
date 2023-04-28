36c94a5a4c33ac4489dd4a19147185989a67d60b
jameskorin <jpkorin@gmail.com>
2023-04-28 16:31:50 -0400

    🙃

--- components/createInvoice.js
+++ components/createInvoice.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Outer } from '../styles/createInvoice'
+import _ from 'lodash'
 
 export default function CreateInvoice({
     visible,
@@ -8,19 +10,77 @@ export default function CreateInvoice({
     const [invoice, setInvoice] = useState({
         name: '',
         due_date: '',
-        charges: []
+        status: '',
+        charges: [{"": ""}]
     });
 
+    useEffect(() => {
+        console.log(invoice.charges);
+    },[invoice])
+
+    const updateCharge =(index, charge)=> {
+        setInvoice(prevState => {
+            let c = [...prevState.charges];
+            c[index] = charge;
+            return {
+                ...prevState,
+                charges: c
+            }
+        })
+    }
+
+    const deleteCharge =(index)=> {
+        setInvoice(prevState => {
+            const updatedCharges = [...prevState.charges];
+            updatedCharges.splice(index, 1);
+            return {
+                ...prevState,
+                charges: updatedCharges,
+            };
+        });
+    }
+
     if(!visible) return null;
-    return <div>
+    return <Outer>
+
+        <h1>New Invoice</h1>
+
         <form onSubmit={e => {
             e.preventDefault();
             createInvoice(invoice)
         }}>
-            <input placeholder='Name'/>
-            <input placeholder='Due date' type='date'/>
-            <input placeholder='Name'/>
+            <input onChange={e => setInvoice({...invoice, name: e.target.value})} 
+            placeholder='Name'/>
+            <select onChange={e => setInvoice({...invoice, status: e.target.value.toLowerCase()})}>
+                <option disabled selected>Status</option>
+                <option>Draft</option>
+                <option>Paid</option>
+                <option>Outstanding</option>
+            </select>
+            <input onChange={e => setInvoice({...invoice, due_date: e.target.value})} 
+            placeholder='Due date' type='date'/>
+
+            <div>
+                <h2>Charges</h2>
+                <button onClick={() => setInvoice({
+                ...invoice, 
+                charges: invoice.charges.concat([{"":""}])})}>
+                    + Add charge
+                </button>
+            </div>
+            {invoice.charges.map((item,index) => {
+                const key = Object.keys(item)[0];
+                const value = parseInt(Object.values(item)[0]);
+                return <div key={`key_${index}`}>
+                    <input placeholder='Name' value={key}
+                    onChange={e => updateCharge(index, {[e.target.value]:value})}/>
+                    $<input placeholder="100" type='number' value={value} 
+                    onChange={e => updateCharge(index, {[key]:e.target.value.toString()})}/>
+                    <button onClick={() => deleteCharge(index)}>Delete</button>
+                </div>
+            })}
+
             <button type='submit'>Create</button>
         </form>
-    </div>;
+    </Outer>;
 }
---
+++ styles/createInvoice.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Outer = styled.div`
+    position: fixed;
+    top: 0px;
+    width: 80vw;
+    max-width: 600px;
+    right: calc(100vw - 100% - 15px);
+    box-shadow: -4px 0px 10px rgba(0,0,0,0.25);
+    height: 100vh;
+    background: #FFFFFF;
+    padding: 10px;
+`;
